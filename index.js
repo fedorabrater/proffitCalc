835199614574bbfe9f4aa166873e6b3b8bf01aad
adalbertobrant <adalbertobrant@gmail.com>
2020-08-16 10:33:47 -0300

    continuando os exercícios do freeCodeCamp arquivo .env criado

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
 
-const absolutePath = __dirname + '/views/index.html';
+//endereço da aplicação
+const absolutepath = __dirname + '/views/index.html';
 
+// mudando o endereço visível da aplicação
+app.use(express.static(__dirname + '/public'));
+
+// landing page da aplicação
 app.get('/', function (req, res) {
-  res.sendFile(absolutePath);
+  res.sendFile(absolutepath);
+});
+
+// api para enviar json
+
+app.get('/json', (req, res) => {
+  res.json({
+    message: 'Hello json',
+  });
 });
 
 app.listen(process.env.PORT || 3000);
